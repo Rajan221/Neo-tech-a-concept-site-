95785d22ffc3b10681b80ed3dcfb87d6b97bf876
Rajan221 <rajansuper221@gmail.com>
2021-10-03 00:47:05 +0545

    first

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// $(window).on('load', function () {
+//   $('#status').delay(800).fadeOut(800);
+//   $('#preloader').delay(1000).fadeOut(1000);
+// });
+
+$(document).ready(function() {
+  $('#first').lightSlider({
+      autoWidth:true,
+      loop: true,
+      auto:true,
+      controls: false,
+      pauseOnHover:true,
+      pause:2000,
+      onSliderLoad: function() {
+          $('#first').removeClass('first');
+      } 
+  });  
+});
+
+$(document).ready(function() {
+  $('#second').lightSlider({
+      autoWidth:true,
+      
+      auto:true,
+      controls: false,
+      pause:2500,
+      onSliderLoad: function() {
+          $('#second').removeClass('second');
+      } 
+  });  
+});
+
+$(document).ready(function() {
+  $('#third').lightSlider({
+      autoWidth:true,
+      
+      auto:true,
+      controls: false,
+      pause:2300,
+      onSliderLoad: function() {
+          $('#third').removeClass('third');
+      } 
+  });  
+});
+
+$(document).ready(function() {
+  $('#fourth').lightSlider({
+      autoWidth:true,
+      
+      auto:true,
+      controls: false,
+      pause:2000,
+      onSliderLoad: function() {
+          $('#fourth').removeClass('fourth');
+      } 
+  });  
+});
+
+$(document).ready(function() {
+  $('#fourthend').lightSlider({
+      autoWidth:true,
+      
+      auto:true,
+      controls: false,
+      pause:2000,
+      onSliderLoad: function() {
+          $('#fourthend').removeClass('fourthend');
+      } 
+  });  
+});
+
+
+
+
+
